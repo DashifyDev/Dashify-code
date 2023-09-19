cfc9bd10a5f60dfc2a27b518bdbb043047fe6c13
ayushi_jain <ayushi.jain@ongraph.com>
2023-09-19 11:18:34 +0530

    admin routing bugs

--- src/hooks/isAdmin.js
+++ src/hooks/isAdmin.js
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 export default function useAdmin() {
-    const { user } = useUser();
-    const [adminState, setAdminState] = useState(false);
+    const { user,isLoading } = useUser();
+    const [adminState, setAdminState] = useState(null);
 
     useEffect(()=>{
-        if (user) {
-            if (user["https://www.boardzy.app/roles"].includes("admin")) {
-              setAdminState((prev)=>{return true})
-            }
-        
-          }
-    },[user])
+      if (user && !isLoading) {
+        if (user["https://www.boardzy.app/roles"].includes("admin")) {
+          setAdminState((prev)=>{return true})
+        }
+        else{
+          setAdminState((prev)=>{return false})
+        }     
+      }
+      else if( user === undefined && !isLoading){
+        setAdminState((prev)=>{return false})
+      }
+    },[user,isLoading])
   return adminState;
 }
