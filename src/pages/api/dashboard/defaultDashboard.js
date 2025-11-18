93f034867ef955782b6cba4be71f1b23d337c823
FedynaM <maturepaxtonxq@gmail.com>
2025-11-18 13:41:19 +0200

    fix(api): filter default dashboards by contact@boardzy.app user only

--- src/pages/api/dashboard/defaultDashboard.js
+++ src/pages/api/dashboard/defaultDashboard.js
@@ -1,23 +1,30 @@
 import connectMongo from "@/utils/db";
 import Dashboard from "@/models/dashboard";
 import Tile from "@/models/tile";
+import User from "@/models/user";
 
-const defaultDashboard= async(req,res)=>{
-    try {
-        await connectMongo()
-        switch (req.method) {
-            case 'GET':
-                let adminBoards= await Dashboard.find({hasAdminAdded:true}).populate("tiles")
-                if(adminBoards){
-                    res.status(200).send(adminBoards)
-                }else{
-                    res.status(400).send("No Admin Board Found")
-                }
+const defaultDashboard = async (req, res) => {
+  try {
+    await connectMongo();
+    switch (req.method) {
+      case "GET":
+        const adminUser = await User.findOne({ email: "contact@boardzy.app" });
+        if (!adminUser) {
+          return res.status(400).send("Admin user not found");
         }
-        
-    } catch (error) {
-        console.log("Error:",error.message);
+
+        let adminBoards = await Dashboard.find({
+          hasAdminAdded: true,
+          userId: adminUser._id,
+        }).populate("tiles");
+        if (adminBoards) {
+          res.status(200).send(adminBoards);
+        } else {
+          res.status(400).send("No Admin Board Found");
+        }
     }
-    
-}
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+};
 export default defaultDashboard;
